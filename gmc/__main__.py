d23999f52d4d62615802d5d52672886cb31d1246
Christian Schudoma (cschu) <christian.schudoma@earlham.ac.uk>
2019-02-21 20:08:44 +0000

    basic singularity call to mikado configure working, creates configuration file

--- gmc/__main__.py
+++ gmc/__main__.py
@@ -8,7 +8,7 @@ import subprocess
 
 from collections import OrderedDict
 
-def parseHints(fn):
+def parseListFile(fn):
 	d = OrderedDict()
 	for row in csv.reader(open(fn), delimiter="\t"):
 		d[row[1]] = row[0], bool(row[2]), int(row[3]), bool(row[4])
@@ -70,23 +70,72 @@ def createScoringFile(fn, hints, fo):
 def main():	
 
 	ap = argparse.ArgumentParser()
-	ap.add_argument("hints_file", type=str)
+	ap.add_argument("list_file", type=str)
 	ap.add_argument("scoring_template", type=str)
 
+	ap.add_argument("--external", type=str, default="")
 	ap.add_argument("--outdir", "-o", type=str, default="gmc_run")
 	ap.add_argument("--mikado-container", type=str, default="/ei/software/testing/gmc/dev/x86_64/mikado.simg")
-	
+
+	"""
+	--external EXTERNAL   External configuration file to overwrite/add values from.
+                            Parameters specified on the command line will take precedence over those present in the configuration file.	
+	--list LIST           List of the inputs, one by line, in the form:
+                        <file1>  <label>  <strandedness (true/false)> <bonus/malus (default 0)>
+	 --reference REFERENCE
+                        Fasta genomic reference.
+	-od OUT_DIR, --out-dir OUT_DIR
+                        Destination directory for the output.
+	--scoring SCORING
+
+
+	-i INTRON_RANGE INTRON_RANGE, --intron-range INTRON_RANGE INTRON_RANGE
+                        Range into which intron lengths should fall, as a couple of integers.
+                                                     Transcripts with intron lengths outside of this range will be penalised.
+                                                     Default: (60, 900)
+	  --pad                 Whether to pad transcripts in loci.
+
+	--junctions JUNCTIONS
+	  -bt BLAST_TARGETS, --blast_targets BLAST_TARGETS
+
+  --daijin              Flag. If set, the configuration file will be also valid for Daijin.
+  -bc BLAST_CHUNKS, --blast-chunks BLAST_CHUNKS
+                        Number of parallel DIAMOND/BLAST jobs to run. Default: 10.
+  --use-blast           Flag. If switched on, Mikado will use BLAST instead of DIAMOND.
+  --use-transdecoder    Flag. If switched on, Mikado will use TransDecoder instead of Prodigal.
+  --mode {nosplit,stringent,lenient,permissive,split} [{nosplit,stringent,lenient,permissive,split} ...]
+                        Mode(s) in which Mikado will treat transcripts with multiple ORFs.
+                        - nosplit: keep the transcripts whole.
+                        - stringent: split multi-orf transcripts if two consecutive ORFs have both BLAST hits
+                                     and none of those hits is against the same target.
+                        - lenient: split multi-orf transcripts as in stringent, and additionally, also when
+                                   either of the ORFs lacks a BLAST hit (but not both).
+                        - permissive: like lenient, but also split when both ORFs lack BLAST hits
+                        - split: split multi-orf transcripts regardless of what BLAST data is available.
+                        If multiple modes are specified, Mikado will create a Daijin-compatible configuration file.
+	"""
 	args = ap.parse_args()
 	
 
-	hints = parseHints(args.hints_file)
+	listFile = parseListFile(args.list_file)
 
 	pathlib.Path(args.outdir).mkdir(exist_ok=True, parents=True)
+	scoringFile = os.path.join(args.outdir, "scoring_test.yaml")
 
-	createScoringFile(args.scoring_template, hints, os.path.join(args.outdir, "scoring.test.yaml"))
+	createScoringFile(args.scoring_template, listFile, scoringFile)
 
+	" mikado configure --list list.txt --reference chr5.fas --mode permissive --scoring plants.yaml  --copy-scoring plants.yaml --junctions junctions.bed -bt uniprot_sprot_plants.fasta configuration.yaml"
 
-	cmd = "singularity exec {} mikado configure -h".format(args.mikado_container)
+	cmd = "singularity exec {} mikado configure --list {} {} -od {} --scoring {} {}".format(
+		args.mikado_container,
+		args.list_file,
+		("--external " + args.external) if args.external else "",
+		args.outdir,
+		scoringFile,
+		os.path.abspath("mikado_config.yaml")
+	)
+
+	print(cmd)
 	out = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
 
 	print(out.decode(), sep="\n")
@@ -98,18 +147,3 @@ def main():
 
 if __name__ == "__main__": 
 	main()
-"""
-scoring:
-  # external metrics START
-  # external.tpsi_cov: {rescaling: max, use_raw: true, multiplier: 10}
-  # external.all_repeats_cov: {rescaling: max, use_raw: true, multiplier: 10}
-  # external.interspersed_repeats_cov: {rescaling: max, use_raw: true, multiplier: 10}
-  external.cpc: {rescaling: max, use_raw: true, multiplier: 1}
-  # all boolean metrics values from here below
-  # external.EI_tpm_05: {rescaling: max, use_raw: true, multiplier: 10}
-  # external.EI_tpm_1: {rescaling: max, use_raw: true, multiplier: 10}
-  # external.SRA_tpm_05: {rescaling: max, use_raw: true, multiplier: 10}
-  # external.SRA_tpm_1: {rescaling: max, use_raw: true, multiplier: 10}
-  external.fln: {rescaling: max, use_raw: true, multiplier: 5}
-  # external metrics END
-"""
