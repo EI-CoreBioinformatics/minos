866ecafc3f953dcf7b021fb162dfd2715db710a0
Christian Schudoma (cschu) <christian.schudoma@earlham.ac.uk>
2019-02-28 19:16:36 +0000

    Changed status of rule gmc_mikado_prepare to live run

--- gmc/zzz/gmc_run.smk.py
+++ gmc/zzz/gmc_run.smk.py
@@ -1,18 +1,31 @@
 import os
 
-localrules: all, gmc_mikado_prepare
+localrules: all
 
 rule all:
 	input:
-		[os.path.join(config["outdir"], "DUMMY")]
+		[os.path.join(config["outdir"], "MIKADO_PREPARE_DONE")]
 
 rule gmc_mikado_prepare:
 	input:
 		config["mikado-config-file"]
 	output:
-		os.path.join(config["outdir"], "DUMMY")
+		os.path.join(config["outdir"], "MIKADO_PREPARE_DONE")
 	params:
-		mikado = config["mikado-container"] + " mikado prepare"
+		mikado = config["mikado-container"] + " mikado prepare",
+		min_length = 100,
+		outdir = config["outdir"]
+	log:
+		os.path.join(config["outdir"], "logs", config["prefix"] + ".mikado_prepare.log")
+	threads:
+		30
 	shell:
-		"singularity exec {params.mikado} -h && touch {output[0]}"
+		"singularity exec {params.mikado} --minimum_length {params.min_length} --json-conf {input[0]} --procs {threads} -od {params.outdir} &> {log} && touch {output[0]}"
+
+
+
+#WD: /ei/workarea/group-ga/Projects/CB-GENANNO-430_Annotation_of_Melia_azedarach_and_Quillaja_saponaria/Analysis/Quillaja_saponaria/mikado-1.2.4/annotation_run1/run1
+#CMD:
+#sbatch -p ei-medium -c 30 --mem 120G -o out_mikado.prepare.%j.log -J QS_Mikado_prepare --wrap "source mikado-devel && /usr/bin/time -v mikado prepare --minimum_length 100 --procs 30 --json-conf Quillaja_saponaria.configuration.yaml"
+
 		
