9ee1a93c157915974aef174923e1ae008d7f591b
Christian Schudoma (cschu) <christian.schudoma@earlham.ac.uk>
2019-02-28 17:39:31 +0000

    Cleanup and added run handling

--- gmc/__main__.py
+++ gmc/__main__.py
@@ -3,8 +3,10 @@ import sys
 import yaml
 import argparse
 import os
+from os.path import join, basename, dirname, abspath
 import pathlib
 import subprocess
+import glob
 
 from collections import OrderedDict
 
@@ -16,7 +18,7 @@ from .snakemake_helper import *
 def add_default_options(parser):
 	common_group = parser.add_argument_group("gmc options")
 			
-	common_group.add_argument("list_file", type=str)
+	# common_group.add_argument("list_file", type=str)
 	common_group.add_argument("--outdir", "-o", type=str, default="gmc_run")
 	common_group.add_argument("--prefix", type=str, default="gmc_run")
 	common_group.add_argument("--mikado-container", type=str, default="/ei/software/testing/gmc/dev/x86_64/mikado.simg")
@@ -29,9 +31,9 @@ def add_configure_parser(subparsers):
 		description=""
 	)
 	
+	configure_parser.add_argument("list_file", type=str)	
 	configure_parser.add_argument("scoring_template", type=str)
 	configure_parser.add_argument("--external", type=str, default="")
-
 	
 	add_default_options(configure_parser)
 	configure_parser.set_defaults(runmode="configure")
@@ -66,76 +68,25 @@ def main():
 	add_configure_parser(subparsers)
 	add_run_parser(subparsers)
 
-
-	"""
-	--external EXTERNAL   External configuration file to overwrite/add values from.
-                            Parameters specified on the command line will take precedence over those present in the configuration file.	
-	--list LIST           List of the inputs, one by line, in the form:
-                        <file1>  <label>  <strandedness (true/false)> <bonus/malus (default 0)>
-	 --reference REFERENCE
-                        Fasta genomic reference.
-	-od OUT_DIR, --out-dir OUT_DIR
-                        Destination directory for the output.
-	--scoring SCORING
-
-
-	-i INTRON_RANGE INTRON_RANGE, --intron-range INTRON_RANGE INTRON_RANGE
-                        Range into which intron lengths should fall, as a couple of integers.
-                                                     Transcripts with intron lengths outside of this range will be penalised.
-                                                     Default: (60, 900)
-	  --pad                 Whether to pad transcripts in loci.
-
-	--junctions JUNCTIONS
-	  -bt BLAST_TARGETS, --blast_targets BLAST_TARGETS
-
-  --daijin              Flag. If set, the configuration file will be also valid for Daijin.
-  -bc BLAST_CHUNKS, --blast-chunks BLAST_CHUNKS
-                        Number of parallel DIAMOND/BLAST jobs to run. Default: 10.
-  --use-blast           Flag. If switched on, Mikado will use BLAST instead of DIAMOND.
-  --use-transdecoder    Flag. If switched on, Mikado will use TransDecoder instead of Prodigal.
-  --mode {nosplit,stringent,lenient,permissive,split} [{nosplit,stringent,lenient,permissive,split} ...]
-                        Mode(s) in which Mikado will treat transcripts with multiple ORFs.
-                        - nosplit: keep the transcripts whole.
-                        - stringent: split multi-orf transcripts if two consecutive ORFs have both BLAST hits
-                                     and none of those hits is against the same target.
-                        - lenient: split multi-orf transcripts as in stringent, and additionally, also when
-                                   either of the ORFs lacks a BLAST hit (but not both).
-                        - permissive: like lenient, but also split when both ORFs lack BLAST hits
-                        - split: split multi-orf transcripts regardless of what BLAST data is available.
-                        If multiple modes are specified, Mikado will create a Daijin-compatible configuration file.
-	"""
 	args = ap.parse_args()
 
 
 	print(args.runmode)
 	if args.runmode == "configure":
 		run_configure(args)	
-	"""
-	listFile = parseListFile(args.list_file)
+	elif args.runmode == "run":
+		snake = join(dirname(__file__), "zzz", "gmc_run.smk.py")
+		try:
+			run_config = abspath(glob.glob(os.path.join(args.outdir, "*.run_config.yaml")).pop())
+		except:
+			raise ValueError("Missing run configuration in " + args.outdir)
 
-	pathlib.Path(args.outdir).mkdir(exist_ok=True, parents=True)
-	scoringFile = os.path.join(args.outdir, args.prefix + ".scoring.yaml")
+		print("Using run configuration file: " + run_config)
 
-	createScoringFile(args.scoring_template, listFile, scoringFile)
+		exe_env = ExecutionEnvironment(args, NOW, job_suffix="GMC_" + args.outdir, log_dir=os.path.join(args.outdir, "hpc_logs"))
 
-	" mikado configure --list list.txt --reference chr5.fas --mode permissive --scoring plants.yaml  --copy-scoring plants.yaml --junctions junctions.bed -bt uniprot_sprot_plants.fasta configuration.yaml"
-
-	cmd = "singularity exec {} mikado configure --list {} {} -od {} --scoring {} {}".format(
-		args.mikado_container,
-		args.list_file,
-		("--external " + args.external) if args.external else "",
-		args.outdir,
-		scoringFile,
-		os.path.abspath("mikado_config.yaml")
-	)
-
-	print(cmd)
-	out = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
-
-
-	with open(os.path.join(args.outdir, args.prefix + ".config.yaml"), "wt") as config_out:
-		print(out.decode(), sep="\n", file=config_out)
-	"""
+		run_snakemake(snake, args.outdir, run_config, exe_env)
+	
 
 	pass
 
