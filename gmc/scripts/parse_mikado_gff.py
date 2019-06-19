d7c876df4cada6715ed4a29d4e9fdf983d342086
Christian Schudoma (cschu) <christian.schudoma@earlham.ac.uk>
2019-06-19 17:12:24 +0100

    Fixed behviour for multiple aliases (1st retained) and introduced oo-code for gff processing.

--- gmc/scripts/parse_mikado_gff.py
+++ gmc/scripts/parse_mikado_gff.py
@@ -26,66 +26,116 @@ def get_alias(tid, attrib):
 				print(err_msg.format("prev_parent", tid, "Falling back to ID ... "), end="", file=sys.stderr)
 				alias = tid
 
-	return alias
+	return alias[0] if type(alias) is list else alias
+
+
+class GffAttributes(dict):
+	def __init__(self, attribs):
+		for item in attribs.split(";"):
+			try:
+				k, v = item.split("=")
+			except:
+				pass
+			self.setdefault(k, list()).append(v)
+
+	def __str__(self):
+		return ";".join("{}={}".format(k, v[0]) for k, v in self.items())
+				
+		
+
+class GffRecord():
+	__fields = ["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"]
+	
+	def __init__(self, data):
+		try:
+			for k, v in zip(self.__fields[:-1], data[:-1]):
+				setattr(self, k, v)
+		except:
+			raise ValueError("Error when parsing gff line:\n{}\n".format("\t".join(data)))
+
+		self.attributes = GffAttributes(data[-1])
+
+	def is_feature(self, feature):
+		return self.type.lower() == feature.lower()
+
+	def get_attribute(self, attr_id):
+		try:
+			return self.attributes[attr_id]
+		except KeyError:
+			raise KeyError("Error: Could not find attribute {} in\n{}\n".format(attr_id, str(self)))
+
+	def normalize_feature(self):
+		norm_map = {
+			"sublocus": "gene",
+			"ncrna_gene": "gene",
+			"mrna": "mRNA",
+			"ncrna": "mRNA",
+			"transcript": "mRNA"
+		}
+		
+		if self.type.lower() == "sublocus":
+			self.attributes["superlocus"] = self.get("Parent", ".")
+			try:
+				del self["Parent"]
+			except:
+				pass
+
+		try:
+			self.type = norm_map[self.type.lower()]
+		except:
+			raise ValueError("Error: Invalid feature {} type in \n{}\n".format(self.type, str(self)))
+
+	def __str__(self):
+		sentinel = "###\n" if self.is_feature("gene") else ""			
+		return sentinel + "\t".join(str(getattr(self, attr)) for attr in self.__fields)
+
+
+class MikadoLociParser():
+	@staticmethod
+	def parse(gff_file, source="."):
+		for row in csv.reader(open(gff_file), delimiter="\t"):
+			if row[0]:
+				if row[0].startswith("#"):
+					print(*row, sep="\t")
+				else:
+					gff_rec = GffRecord(row)
+					gff_rec.source = source
+	
+					if gff_rec.is_feature("superlocus"):
+						continue
+					elif gff_rec.is_feature("sublocus"):
+						gff_rec.normalize_feature()
+					elif gff_rec.is_feature("ncRNA_gene"):
+						gff_rec.normalize_feature()
+					elif any(gff_rec.is_feature(feat) for feat in ("mRNA", "ncRNA", "transcript")):
+						gff_rec.normalize_feature()
+						tid = gff_rec.get_attribute("ID")[0]
+						
+						preserve_attribs = ("ID", "Parent", "Name", "alias", "Note")
+						new_attrib = {
+							"ID": tid,
+							"Parent": gff_rec.get_attribute("Parent")[0],
+							"Name": get_alias(tid, gff_rec.attributes),
+							"Note": [tid]
+						}
+						new_attrib["Note"].extend("{}:{}".format(k, v[0]) for k, v in gff_rec.attributes.items() if k not in preserve_attribs and k.lower() != "note")
+						new_attrib["Note"].extend(gff_rec.attributes.get("note", [""])[0].split("|")[1:])
+						new_attrib["Note"] = ",".join(new_attrib["Note"])
+						gff_rec.attributes = ";".join("{}={}".format(k, v) for k, v in new_attrib.items())
+					
+						
+					print(gff_rec)
+
 
 
 def main():
 
 	ap = argparse.ArgumentParser()
 	ap.add_argument("gff", type=str)
-	ap.add_argument("--source", type=str, default="Mikado_annotation_run2")
+	ap.add_argument("--source", type=str, default="Mikado_annotation_run2")	
 	args = ap.parse_args()
 
-	for row in csv.reader(open(args.gff), delimiter="\t"):
-		if row[0]:
-			if not row[0].startswith("#"):
-				feature = row[2]
-				row[1] = args.source
-				if feature == "superlocus":
-					continue
-				if feature == "sublocus":
-					row[2] = "gene"
-					row[8] = row[8].replace("Parent=", "superlocus=")
-					print("###")
-				elif feature == "ncRNA_gene":
-					row[2] == "gene"
-				elif feature in {"mRNA", "ncRNA", "transcript"}:
-					row[2] = "mRNA"
-					attrib = dict(p.split("=") for p in row[8].split(";"))
-					tid = attrib.get("ID", None)
-					if tid is None:
-						raise ValueError("Could not find transcript id.\n{}\n".format(*row, sep="\t"))
-
-					alias = get_alias(tid, attrib)
-					preserve_attribs = ("ID", "Parent", "Name", "alias", "Note")
-					new_attrib = {
-						"ID": tid,
-						"Parent": attrib["Parent"],
-						"Name": alias,
-						"Note": [tid]
-					}
-
-					# new_attrib = collections.OrderedDict((k, attrib.get(k)) for k in preserve_attribs)
-					# new_attrib["alias"] = alias
-					# new_attrib["Note"] = [tid]
-					new_attrib["Note"].extend("{}:{}".format(k, v) for k, v in attrib.items() if k not in preserve_attribs and k.lower() != "note")
-					new_attrib["Note"].extend(attrib.get("note", "").split("|")[1:])
-					new_attrib["Note"] = ",".join(new_attrib["Note"])
-					row[8] = ";".join("{}={}".format(k, v) for k, v in new_attrib.items())
-				
-					
-			print(*row, sep="\t")
-					 
-
-
-			
-			
-	
-
-
-
-	pass
-
+	MikadoLociParser.parse(args.gff, source=args.source)
 
 if __name__ == "__main__": 
 	main()
