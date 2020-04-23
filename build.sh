80638e5c7987f6b75b7ca8fd11d720c0eceaefaa
Christian Schudoma (cschu) <christian.schudoma@earlham.ac.uk>
2020-04-23 22:36:15 +0100

    Activate te_score in classification (issue #18)

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ version=$1
 
 #rm dist/*whl
 python setup.py bdist_wheel
-pip install --prefix=/ei/software/testing/gmc/${version}/x86_64 -U dist/gmc-${version}-*.whl
+pip install --prefix=/ei/software/cb/gmc/${version}/x86_64 -U dist/gmc-${version}-*.whl
 #pip install --install-option="--prefix=/ei/software/testing/gmc/${version}/x86_64" -U dist/gmc-${version}-*.whl
