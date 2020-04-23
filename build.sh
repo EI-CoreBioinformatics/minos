1e3d1797b99143abe2ba1ef405172f83b9e1b65c
Christian Schudoma (cschu) <christian.schudoma@earlham.ac.uk>
2020-04-23 10:30:43 +0100

    Refactored build.sh - build.sh now uses version string from setup.py if none supplied as first argument - if version string is supplied (backwards compatibility) it checks it against the version in setup.py - install path prefix can be supplied as second argument to build.sh - ./build.sh without arguments now installs to default location

--- build.sh
+++ build.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-version=$1
+install_prefix=/ei/software/cb/gmc
+expected_version=$(grep -o "version *= *[\".0-9]\+" setup.py | sed "s/[\" ]//g" | cut -f 2 -d =)
+
+
+if [[ -z "$expected_version" ]]; then 
+ echo "Cannot parse version from setup.py. Please make sure to set a version (int)."
+ exit
+elif [[ "$#" -ge 1 ]]; then
+  if [[ "$1" == "$expected_version" ]]; then
+    version=$1
+    if [[ "$#" -ge 2 ]]; then
+      install_prefix=$2
+    fi
+  else
+    echo "Supplied version ($1) does not match expected version ($expected_version)."
+    exit
+  fi
+else
+  version=$expected_version
+fi
+
+echo "Installing to $install_prefix/$version"
+
+
 
 #rm dist/*whl
 python setup.py bdist_wheel
-pip install --prefix=/ei/software/cb/gmc/${version}/x86_64 -U dist/gmc-${version}-*.whl
+pip install --prefix=${install_prefix}/${version}/x86_64 -U dist/gmc-${version}-*.whl
 #pip install --install-option="--prefix=/ei/software/testing/gmc/${version}/x86_64" -U dist/gmc-${version}-*.whl
