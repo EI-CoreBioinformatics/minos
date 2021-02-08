1bfb400df33b7ea570fb5669ab2f8b9340a8568d
Gemy Kaithakottil <gemygk@gmail.com>
2021-02-08 16:57:57 +0000

    refactored code #45

--- minos/scripts/calculate_cdslen.py
+++ minos/scripts/calculate_cdslen.py
@@ -14,14 +14,10 @@ def calculate_cdslen(_in_cds, _in_cdna, _out, min_cds_length):
 			yield sid, "".join(seq)
 
 	with open(_out, "w") as fout:
-		cdna_set = set()
-		for sid, seq in read_fasta(_in_cdna):
-			if sid not in cdna_set:
-				cdna_set.add(sid)
+		cdna_set = set(sid for sid, _ in read_fasta(_in_cdna))
 		cds_set = set()
 		for sid, seq in read_fasta(_in_cds):
-			if sid not in cds_set:
-				cds_set.add(sid)
+			cds_set.add(sid)
 			seq = seq.upper().replace("U", "T")
 			has_stop = any(seq.endswith(stop_codon) for stop_codon in ("TAA", "TGA", "TAG"))
 			discard = (has_stop and len(seq) < min_cds_length) or (not has_stop and len(seq) < min_cds_length - 3)
@@ -29,5 +25,4 @@ def calculate_cdslen(_in_cds, _in_cdna, _out, min_cds_length):
 
 		# get the ncRNA models and add them to CDS discard list
 		for sid in cdna_set - cds_set:
-			discard = True
-			print(sid, int(discard), sep="\t", flush=True, file=fout)
+			print(sid, 1, sep="\t", flush=True, file=fout)
