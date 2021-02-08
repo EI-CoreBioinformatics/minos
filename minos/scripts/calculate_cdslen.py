283c88537e6f6a916f7d68537aba0de232a89e60
Gemy Kaithakottil <gemygk@gmail.com>
2021-02-08 15:00:12 +0000

    Including ncRNA models to the cds.fasta.lengths file #45

--- minos/scripts/calculate_cdslen.py
+++ minos/scripts/calculate_cdslen.py
@@ -1,4 +1,4 @@
-def calculate_cdslen(_in, _out, min_cds_length):
+def calculate_cdslen(_in_cds, _in_cdna, _out, min_cds_length):
 	def read_fasta(f):
 		sid, seq = None, list()
 		for line in open(f):
@@ -14,8 +14,20 @@ def calculate_cdslen(_in, _out, min_cds_length):
 			yield sid, "".join(seq)
 
 	with open(_out, "w") as fout:
-		for sid, seq in read_fasta(_in):
+		cdna_list = dict()
+		for sid, seq in read_fasta(_in_cdna):
+			if sid not in cdna_list:
+				cdna_list[sid] = 1
+		cds_list = dict()
+		for sid, seq in read_fasta(_in_cds):
+			if sid not in cds_list:
+				cds_list[sid] = 1
 			seq = seq.upper().replace("U", "T")
 			has_stop = any(seq.endswith(stop_codon) for stop_codon in ("TAA", "TGA", "TAG"))
 			discard = (has_stop and len(seq) < min_cds_length) or (not has_stop and len(seq) < min_cds_length - 3)
+			print(sid, int(discard), sep="\t", flush=True, file=fout)
+
+		# get the ncRNA models and add them to CDS discard list
+		for sid, _ in {k: cdna_list[k] for k in set(cdna_list) - set(cds_list)}.items():
+			discard = True
 			print(sid, int(discard), sep="\t", flush=True, file=fout)
