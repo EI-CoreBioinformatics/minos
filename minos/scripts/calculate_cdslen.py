12d8fcde7d79006dbeb2bf88d86a8fdadeb130a2
Gemy Kaithakottil <gemygk@gmail.com>
2021-02-08 15:57:59 +0000

    changed from dict to set, thanks @cschu #45

--- minos/scripts/calculate_cdslen.py
+++ minos/scripts/calculate_cdslen.py
@@ -14,20 +14,20 @@ def calculate_cdslen(_in_cds, _in_cdna, _out, min_cds_length):
 			yield sid, "".join(seq)
 
 	with open(_out, "w") as fout:
-		cdna_list = dict()
+		cdna_set = set()
 		for sid, seq in read_fasta(_in_cdna):
-			if sid not in cdna_list:
-				cdna_list[sid] = 1
-		cds_list = dict()
+			if sid not in cdna_set:
+				cdna_set.add(sid)
+		cds_set = set()
 		for sid, seq in read_fasta(_in_cds):
-			if sid not in cds_list:
-				cds_list[sid] = 1
+			if sid not in cds_set:
+				cds_set.add(sid)
 			seq = seq.upper().replace("U", "T")
 			has_stop = any(seq.endswith(stop_codon) for stop_codon in ("TAA", "TGA", "TAG"))
 			discard = (has_stop and len(seq) < min_cds_length) or (not has_stop and len(seq) < min_cds_length - 3)
 			print(sid, int(discard), sep="\t", flush=True, file=fout)
 
 		# get the ncRNA models and add them to CDS discard list
-		for sid, _ in {k: cdna_list[k] for k in set(cdna_list) - set(cds_list)}.items():
+		for sid in cdna_set - cds_set:
 			discard = True
 			print(sid, int(discard), sep="\t", flush=True, file=fout)
